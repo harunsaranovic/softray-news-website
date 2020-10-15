9ceccfb431a7068c8b9b12ff6687bfa20bdf21b5
Harun Šaranović <harunsaranovic@users.noreply.github.com>
2020-10-15 11:52:52 +0200

    date formatted

--- frontend/src/app/components/news-editor/news-editor.component.ts
+++ frontend/src/app/components/news-editor/news-editor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { NewsService } from '../../services/news.service';
+import { UsersService } from '../../services/users.service';
 import { News } from '../../models/news';
 
 @Component({
@@ -10,16 +11,17 @@ import { News } from '../../models/news';
   styleUrls: ['./news-editor.component.scss']
 })
 export class NewsEditorComponent implements OnInit {
-
   constructor(
     private route: ActivatedRoute,
     public newsService: NewsService,
+    public usersService: UsersService,
     private router: Router
-    ) {}
+  ) {}
 
   news: News;
 
   ngOnInit() {
+    if (this.usersService.loggedUser == false) this.router.navigate(['/login']);
     this.route.queryParams.subscribe(params => {
       const id = params.id;
       this.newsService.getSingleNews(id).subscribe(news => {
@@ -27,7 +29,7 @@ export class NewsEditorComponent implements OnInit {
       });
     });
   }
-  
+
   update() {
     console.log(this.news);
     this.newsService.updateNews(this.news).subscribe(data => {
